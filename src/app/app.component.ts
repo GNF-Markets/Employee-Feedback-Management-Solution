9f0ba965258b81520e524547833ad6a55f448c61
HlelaFono <fonohlela@gmail.com>
2025-03-27 22:31:00 +0200

    Enhance user interface: add conditional toolbars for employee and admin roles, implement logout functionality, and update login state management

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common'; 
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive, Router } from '@angular/router';
 import { DemoAngularMaterialModule } from './DemoAngularMaterialModule';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-
+import { StorageService } from './auth/services/storage/storage.service'; // Adjust the path as needed
 
 @Component({
   selector: 'app-root',
@@ -23,11 +23,30 @@ import { MatButtonModule } from '@angular/material/button';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
 
   selectedForm: string = ''; 
   selectedLanguages: string[] = [];
   title = 'Employee-Feedback-Management-Solution';
+
+  isEmployeeLoggedIn: boolean = StorageService.isEmployeeLoggedIn();
+  isAdminLoggedIn: boolean = StorageService.isAdminLoggedIn();
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.router.events.subscribe(() => {
+      this.isEmployeeLoggedIn = StorageService.isEmployeeLoggedIn();
+      this.isAdminLoggedIn = StorageService.isAdminLoggedIn();
+    });
+  }
+
+  logout(){
+    StorageService.logout();
+    this .router.navigate(['/login']);
+  }
 
   addLanguage() {
     const languageSelect = document.getElementById('languageSelect') as HTMLSelectElement;
