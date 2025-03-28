475a4b794d254e3ce182c03624181d466efc776e
HlelaFono <fonohlela@gmail.com>
2025-03-29 00:29:32 +0200

    Fix API endpoint paths in AdminService and update BASIC_URL format

--- src/app/modules/admin/services/admin.service.ts
+++ src/app/modules/admin/services/admin.service.ts
@@ -5,7 +5,7 @@ import { HttpClientModule } from '@angular/common/http'; // Import HttpClientMod
 import { Observable } from 'rxjs';
 import { StorageService } from '../../../auth/services/storage/storage.service';
 
-const BASIC_URL = 'http://localhost:8080';
+const BASIC_URL = 'http://localhost:8080/';
 @Injectable({
   providedIn: 'root'
 })
@@ -13,18 +13,19 @@ export class AdminService {
 
   constructor(private http: HttpClient) { }
   getUsers():Observable<any>{
-    return this.http.get(BASIC_URL + 'apo/admin/users', {
+    return this.http.get(BASIC_URL + 'api/admin/users', {
     headers: this.createAuthorationHeader(),
    });
   }
   postTask(taskDTO: any): Observable<any>{
-    return this.http.post(BASIC_URL + 'apo/admin/tasks', taskDTO, {
+    return this.http.post(BASIC_URL + 'api/admin/tasks', taskDTO, {
     headers: this.createAuthorationHeader()
    });
   }
   private createAuthorationHeader(): HttpHeaders {
     return new HttpHeaders().set(
       'Authorization', 'Bearer ' + StorageService.getToken()
-    )
+    );
+    
   }
 }
