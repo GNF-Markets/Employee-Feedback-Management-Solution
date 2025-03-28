051e21d720cb4b2915dae576924001eb0a7d9242
samkeleN <samkelendzululeka@gmail.com>
2025-03-28 23:28:58 +0200

    Implement task management dashboard: add task list display, integrate task retrieval from API, and style components

--- src/app/modules/admin/components/dashboard/dashboard.component.ts
+++ src/app/modules/admin/components/dashboard/dashboard.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { AdminService } from '../../services/admin.service';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [    CommonModule,
+    MatCardModule,
+    MatDividerModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
 
+  listOfTasks: any = [];
+
+  constructor(private service: AdminService) {
+    this.getTasks();
+   }
+
+   getTasks() {
+    this.service.getAllTasks().subscribe((res) => {
+      this.listOfTasks = res;
+    }
+    )
+   }
 }
+
--- src/app/modules/admin/services/admin.service.ts
+++ src/app/modules/admin/services/admin.service.ts
@@ -5,21 +5,29 @@ import { HttpClientModule } from '@angular/common/http'; // Import HttpClientMod
 import { Observable } from 'rxjs'; // Adjust the path as needed
 import { StorageService } from '../../../auth/services/storage/storage.service';
 
-const BASIC_URL = 'http://localhost:8080';
+const BASIC_URL = 'http://localhost:8080/';
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
 
   constructor(private http: HttpClient) { }
+
   getUsers():Observable<any>{
-    return this.http.get(BASIC_URL + 'apo/admin/users', {
+    return this.http.get(BASIC_URL + 'api/admin/users', {
     headers: this.createAuthorationHeader(),
    });
   }
+
   private createAuthorationHeader(): HttpHeaders {
     return new HttpHeaders().set(
       'Authorization', 'Bearer ' + StorageService.getToken()
     )
+  }
+
+  getAllTasks():Observable<any>{
+    return this.http.get(BASIC_URL + 'api/admin/tasks', {
+    headers: this.createAuthorationHeader(),
+   });
   }
 }
